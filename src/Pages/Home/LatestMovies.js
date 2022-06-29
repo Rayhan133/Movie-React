16360e9710ca68796994db8051b2fcbb5e60aa31
rayhan133 <rayhanalshorif@gmail.com>
2022-06-29 17:50:40 +0600

    before use react-query

--- src/Pages/Home/LatestMovies.js
+++ src/Pages/Home/LatestMovies.js
@@ -1,55 +1,46 @@
 import "./LatestMovies.css";
 export default function LatestMovies() {
+  const previousSlider = () => {
+    const prev = document.querySelector(".owl-prev");
+    prev.click();
+  };
+  const nextSlider = () => {
+    const next = document.querySelector(".owl-next");
+    next.click();
+  };
+
   return (
     <div className="LatestMovies container text-white ">
       <div className="d-flex justify-content-between LatestMoviesTitleSection">
         <h1 className="LatestMoviesTitle">Latest Movies</h1>
         <div className="mt-2 d-flex">
-          <div className="box">
+          <div className="box" onClick={previousSlider}>
             <i class="fa-solid fa-angle-left"></i>
           </div>
-          <div className="box">
+          <div className="box" onClick={nextSlider}>
             <i class="fa-solid fa-angle-right"></i>
           </div>
         </div>
       </div>
       <hr />
-      <div class="owl-carousel owl-theme">
-        <div class="item">
-          <h4>1</h4>
-        </div>
-        <div class="item">
-          <h4>2</h4>
-        </div>
-        <div class="item">
-          <h4>3</h4>
-        </div>
-        <div class="item">
-          <h4>4</h4>
-        </div>
-        <div class="item">
-          <h4>5</h4>
-        </div>
-        <div class="item">
-          <h4>6</h4>
-        </div>
-        <div class="item">
-          <h4>7</h4>
-        </div>
-        <div class="item">
-          <h4>8</h4>
-        </div>
-        <div class="item">
-          <h4>9</h4>
-        </div>
-        <div class="item">
-          <h4>10</h4>
-        </div>
-        <div class="item">
-          <h4>11</h4>
-        </div>
-        <div class="item">
-          <h4>12</h4>
+
+      <div className="">
+        <div class="owl-carousel owl-theme">
+          <div class="item">
+            <h4>1</h4>
+          </div>
+          <div class="item">
+            <h4>2</h4>
+          </div>
+          <div class="item">
+            <h4>3</h4>
+          </div>
+          <div class="item">
+            <h4>4</h4>
+          </div>
+          <div class="item">
+            <h4>5</h4>
+          </div>
         </div>
       </div>
     </div>
